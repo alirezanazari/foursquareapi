28108f641ac3fa1066d09ef6856aff7922019996
a.nazari <a.nazari@kianiranian.com>
2020-02-04 10:09:11 +0330

    Add - Common logic in base viewModel

--- app/src/main/java/ir/alirezanazari/foursquareapi/ui/BaseViewModel.kt
+++ app/src/main/java/ir/alirezanazari/foursquareapi/ui/BaseViewModel.kt
@@ -2,15 +2,33 @@ package ir.alirezanazari.foursquareapi.ui
 
 import androidx.lifecycle.ViewModel
 import ir.alirezanazari.domain.intractor.UseCase
+import ir.alirezanazari.foursquareapi.internal.SingleLiveEvent
 
 
 open class BaseViewModel(
     private val useCase: UseCase? = null
 ) : ViewModel() {
 
+    val loaderVisibilityListener = SingleLiveEvent<Boolean>()
+    val retryVisibilityListener = SingleLiveEvent<Boolean>()
+    val errorVisibilityListener = SingleLiveEvent<Boolean>()
+    val errorListener = SingleLiveEvent<String>()
+
     override fun onCleared() {
         useCase?.clearDisposable()
         super.onCleared()
+    }
+
+    fun setLoaderState(state: Boolean , isEffectRetry: Boolean){
+        if (state){
+            loaderVisibilityListener.value = true
+            retryVisibilityListener.value = false
+            errorVisibilityListener.value = false
+        }else{
+            loaderVisibilityListener.value = false
+            errorVisibilityListener.value = true
+            retryVisibilityListener.value = isEffectRetry
+        }
     }
 
 }
