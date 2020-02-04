0540f99ce39f381ed6e5d15046c99ef6be1e8f3c
Alireza <a.alirezanazari@gmail.com>
2020-02-04 22:28:43 +0330

    Add - Usecase to viewModel and complete dependency

--- app/src/main/java/ir/alirezanazari/foursquareapi/internal/ModulesDI.kt
+++ app/src/main/java/ir/alirezanazari/foursquareapi/internal/ModulesDI.kt
@@ -3,6 +3,15 @@ package ir.alirezanazari.foursquareapi.internal
 import android.content.Context
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import ir.alirezanazari.data.net.ApiConfig
+import ir.alirezanazari.data.net.NetworkDataManager
+import ir.alirezanazari.data.net.NetworkDataManagerImpl
+import ir.alirezanazari.data.net.RequestInterceptor
+import ir.alirezanazari.data.repository.VenueRepositoryImpl
+import ir.alirezanazari.domain.intractor.GetNearVenueUseCase
+import ir.alirezanazari.domain.repository.VenueRepository
 import ir.alirezanazari.foursquareapi.ui.details.LocationDetailViewModel
 import ir.alirezanazari.foursquareapi.ui.locations.LocationListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -10,8 +19,14 @@ import org.koin.dsl.module
 
 val moduleDI = module {
 
-    factory<FusedLocationProviderClient> { LocationServices.getFusedLocationProviderClient(get<Context>()) }
+    single { RequestInterceptor() }
+    single { ApiConfig(get()) }
+    single<NetworkDataManager> { NetworkDataManagerImpl(get()) }
 
-    viewModel { LocationListViewModel() }
+    factory<VenueRepository> { VenueRepositoryImpl(get()) }
+    factory<FusedLocationProviderClient> { LocationServices.getFusedLocationProviderClient(get<Context>()) }
+    factory { GetNearVenueUseCase( get() , Schedulers.io() , AndroidSchedulers.mainThread()) }
+
+    viewModel { LocationListViewModel(get()) }
     viewModel { LocationDetailViewModel() }
 }
--- app/src/main/java/ir/alirezanazari/foursquareapi/ui/locations/LocationListViewModel.kt
+++ app/src/main/java/ir/alirezanazari/foursquareapi/ui/locations/LocationListViewModel.kt
@@ -1,6 +1,9 @@
 package ir.alirezanazari.foursquareapi.ui.locations
 
+import ir.alirezanazari.domain.intractor.GetNearVenueUseCase
 import ir.alirezanazari.foursquareapi.ui.BaseViewModel
 
-class LocationListViewModel : BaseViewModel() {
+class LocationListViewModel(
+    private val useCase: GetNearVenueUseCase
+) : BaseViewModel() {
 }
